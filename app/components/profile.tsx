bc82c47f2f486f86c0b01e5fe7a8942bdd19846f
Marlin Jayasekera <marlin.j.281@gmail.com>
2022-08-31 13:13:16 -0400

    change profile info layout

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -31,7 +31,7 @@ export default function Profile(props: {
   imageUrl: string | null;
   links: UserLink[] | null;
 }) {
-  const { name, username, description, links } = props;
+  const { name, username, links } = props;
   const social = links
     ? links.map((link) => ({
         name: link.name,
@@ -40,6 +40,9 @@ export default function Profile(props: {
       }))
     : [];
 
+  const description = props.description
+    ? props.description.split(/\n|\\n/)
+    : [];
   return (
     <div className="max-w-7xl mx-auto px-2 sm:px-4 py-10">
       <div className="flex justify-end">
@@ -58,22 +61,32 @@ export default function Profile(props: {
           ))}
         </div>
       </div>
-      <div className="text-center mt-8">
-        <div className="flex">
-          {props.imageUrl ? (
-            <img
-              className="h-24 w-24 mx-auto rounded-full ring-4 ring-white sm:h-28 sm:w-28"
-              src={props.imageUrl}
-              alt={props.username}
-            />
-          ) : (
-            <div className="h-24 w-24 mx-auto rounded-full bg-primary-dark sm:h-28 sm:w-28"></div>
-          )}
+      <div className="mt-8 sm:grid sm:grid-cols-7">
+        <div className="space-y-2 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-4 mt-8 text-center sm:text-left sm:col-start-2 sm:col-end-6">
+          <div className="flex justify-center sm:justify-end space-x-4 lg:space-x-6">
+            {props.imageUrl ? (
+              <img
+                className="h-24 w-24 rounded-full ring-4 ring-white sm:h-28 sm:w-28"
+                src={props.imageUrl}
+                alt={props.username}
+              />
+            ) : (
+              <div className="h-24 w-24 rounded-full bg-primary-dark sm:h-28 sm:w-28"></div>
+            )}
+          </div>
+          <div className="flex space-y-1 justify-center flex-col">
+            <p className="text-2xl tracking-tight font-bold sm:text-3xl sm:tracking-tight lg:tracking-tight">
+              {name || username}
+            </p>
+            {description &&
+              description.length > 0 &&
+              description.map((line, idx) => (
+                <p key={idx} className="text-sm sm:text-base">
+                  {line}
+                </p>
+              ))}
+          </div>
         </div>
-        <p className="mt-3 text-2xl tracking-tight font-bold text-gray-900 sm:text-3xl sm:tracking-tight lg:tracking-tight">
-          {name || username}
-        </p>
-        {description && <p className="max-w-xl mt-5 mx-auto">{description}</p>}
       </div>
     </div>
   );
