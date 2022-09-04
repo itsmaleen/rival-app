1b70e9df14d3d0427ae81be2e84d9e4191609065
Marlin Jayasekera <marlin.j.281@gmail.com>
2022-09-04 16:40:35 -0400

    add gtag

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import type { LoaderArgs, LoaderFunction, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,9 +8,17 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useLocation,
 } from "@remix-run/react";
+import { useEffect } from "react";
 import Navbar from "~/components/navbar";
 import styles from "./styles/app.css";
+
+import * as gtag from "./utils/gtag.client";
+
+type LoaderData = {
+  gaTrackingId: string | undefined;
+};
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -27,13 +36,22 @@ export const loader: LoaderFunction = async ({ request }: LoaderArgs) => {
   ).match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i);
 
   //Returning the isMobileView as a prop to the component for further use.
-  return {
+  return json({
     isMobileView: Boolean(isMobileView),
-  };
+    gaTrackingId: process.env.GA_TRACKING_ID,
+  });
 };
 
 export default function App() {
-  const { isMobileView } = useLoaderData();
+  const { isMobileView, gaTrackingId } = useLoaderData();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId);
+    }
+  }, [location, gaTrackingId]);
+
   return (
     <html lang="en">
       <head>
@@ -41,6 +59,28 @@ export default function App() {
         <Links />
       </head>
       <body>
+        {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
         <Navbar isMobileView={isMobileView} />
         <Outlet />
         <ScrollRestoration />
