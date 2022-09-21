93fa378d29fb8a2bbef6fd2ff376234d50e4310c
Marlin Jayasekera <marlin.j.281@gmail.com>
2022-09-20 22:14:56 -0400

    basic sets shown (need to make it addable / not show by default

--- app/models/set.server.ts
+++ app/models/set.server.ts
@@ -1,3 +1,11 @@
+import type {
+  Set,
+  CollectibleSet,
+  User,
+  CollectibleUser,
+} from "@prisma/client";
+import { prisma } from "~/db.server";
+
 // Get api key for env
 const API_KEY = process.env.POKEMON_TCG_API_KEY;
 // X-Api-Key header for fetch requests
@@ -15,6 +23,36 @@ export type PokemonCard = {
 };
 
 // TODO: Move to saving to Database instead of 1000s of fetch requests
+
+export function getOwnedCardsInSet(ownerId: User["id"]) {
+  return prisma.$queryRaw<
+    CollectibleSet[]
+  >`SELECT * FROM collectible_set WHERE "collectibleId" in (
+    SELECT "collectibleId" FROM collectible_user WHERE "ownerId"=${ownerId}
+    );`;
+}
+
+export function getSetsOwned(ownerId: User["id"]) {
+  return prisma.$queryRaw<Set[]>`SELECT * from sets WHERE id in (
+    SELECT DISTINCT "setId" FROM collectible_set WHERE "collectibleId" in (
+        SELECT "collectibleId" FROM collectible_user WHERE "ownerId"=${ownerId}
+        )
+    )`;
+}
+
+export function getCollectiblesFromSet(setName: string) {
+  // using pivot table CollectibleSet to return collectibles from set using set name
+  return prisma.collectibleSet.findMany({
+    include: {
+      collectible: true,
+    },
+    where: {
+      set: {
+        name: setName,
+      },
+    },
+  });
+}
 
 export async function getCardsFromPokemonSetName(setName: string) {
   const response = await fetch(
