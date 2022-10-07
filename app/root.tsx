66b37d12bc8b624b3bbc03ab964c4f1daa4a7038
Marlin Jayasekera <marlin.j.281@gmail.com>
2022-10-06 20:32:52 -0400

    move hotjar script to head

--- app/root.tsx
+++ app/root.tsx
@@ -70,6 +70,20 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        {!hotjarId ? null : (
+          <script>
+            {`
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:${hotjarId},hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `}
+          </script>
+        )}
       </head>
       <body>
         {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
@@ -93,18 +107,6 @@ export default function App() {
               }}
             />
           </>
-        )}
-        {!hotjarId ? null : (
-          <script>
-            {`(function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:${hotjarId},hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-          </script>
         )}
         <SupabaseProvider supabase={supabase}>
           <>
